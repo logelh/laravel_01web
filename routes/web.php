a5df59476862690d082a2765dc9e3fc20e3f0b02
aaron <logelh@163.com>
2021-04-24 11:28:41 +0800

    模仿

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,12 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', 'StaticPagesController@home');
+/**
+ * 如果要使用<li><a href="{{ route('help') }}">帮助</a></li> 这种方式来调用help路由,则需要在路由后面链式调用 name 方法来为路由指定名称
+ * name 命名路由’
+ */
+Route::get('/help', 'StaticPagesController@help')->name('help');
+Route::get('/about', 'StaticPagesController@about')->name('about');
+
+Route::get('signup', 'UserController@create')->name('signup');
--- webpack.mix.js
+++ webpack.mix.js
@@ -4,14 +4,15 @@ const mix = require('laravel-mix');
  |--------------------------------------------------------------------------
  | Mix Asset Management
  |--------------------------------------------------------------------------
- |
+ | mix 管理前端代码。如果需要修改webpack.mix.js 则需要修改ctrl+c 后重新载入才能修改
  | Mix provides a clean, fluent API for defining some Webpack build steps
- | for your Laravel applications. By default, we are compiling the CSS
+ | for your Laravel application. By default, we are compiling the Sass
  | file for the application as well as bundling up all the JS files.
- |
+ | 有一个问题.如果遇到报错。直接删除node-modules 重新下来
  */
 
 mix.js('resources/js/app.js', 'public/js')
-    .postCss('resources/css/app.css', 'public/css', [
-        //
-    ]);
+    .sass('resources/sass/app.scss', 'public/css')
+    // .sourceMaps()
+    .version()
+;
